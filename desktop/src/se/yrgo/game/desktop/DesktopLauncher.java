add2267be64da5b0b8dcda71254caff3f777fab9
Hampus Ram <hampus.ram@educ.goteborg.se>
2025-03-04 15:58:18 +0100

    updated to lwjgl3

--- desktop/src/se/yrgo/game/desktop/DesktopLauncher.java
+++ desktop/src/se/yrgo/game/desktop/DesktopLauncher.java
@@ -1,15 +1,15 @@
 package se.yrgo.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import se.yrgo.game.AlienGame;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+import se.yrgo.game.*;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		// this is where we configure how our program starts on the desktop
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800;
-		config.height = 400;
-		new LwjglApplication(new AlienGame(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(800, 400);
+		new Lwjgl3Application(new AlienGame(), config);
 	}
 }
